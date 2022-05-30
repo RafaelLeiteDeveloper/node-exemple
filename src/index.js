fad8e9f2f6538d7dc54ac8c7bff3c6a3f735c0bb
Rafael Leite <rafael.leite@solution4you.com.br>
2022-05-30 11:01:45 -0300

    inclusao

--- src/index.js
+++ src/index.js
@@ -2,40 +2,48 @@ import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
 
-import models from './models';
 import routes from './routes';
 
+let shortUrl = require('node-url-shortener');
+
 const app = express();
-
-// * Application-Level Middleware * //
-
-// Third-Party Middleware
-
 app.use(cors());
-
-// Built-In Middleware
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Custom Middleware
 
-app.use((req, res, next) => {
-  req.context = {
-    models,
-    me: models.users[1],
-  };
-  next();
-});
+app.post('/shortener-serveless', 
+  async (req, res) => {
+
+    console.log(req.body.url)
+
+    const urlEncoder = htmlEncoder(req.body.url);
+
+    const promise = new Promise(function(resolve, reject) {
+         shortUrl.short(urlEncoder, function(err, url){
+          resolve(url)
+        });
+    })
+
+    promise.then(obj => {
+      return res.status(200).json({
+        message: obj
+    });
+    })
+
+  },
+);
+
+const htmlEncoder = (url) => {
+  url = url.replace('&', '%26');
+  url = url.replace('?', '%3F');
+  return url;
+}
 
 // * Routes * //
-
-app.use('/session', routes.session);
-app.use('/users', routes.user);
-app.use('/messages', routes.message);
+app.use('/shortener-serveless', routes.short);
 
 // * Start * //
-
 app.listen(process.env.PORT, () =>
   console.log(`Example app listening on port ${process.env.PORT}!`),
 );
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,5 @@
-import session from './session';
-import user from './user';
-import message from './message';
+import short from './message';
 
 export default {
-  session,
-  user,
-  message,
+  short
 };
